905ac78e2cf54c41022df9481a283ea998014c0a
Viswanadham <vvv3@login06.o2.rc.hms.harvard.edu>
2019-10-06 20:39:08 -0400

    install packages required

--- install_packages_required.R
+++ install_packages_required.R
@@ -1,6 +1,17 @@
 if (!requireNamespace("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 
-
 # install rtracklayer
 BiocManager::install("rtracklayer")
+
+# install Rsamtools
+BiocManager::install("Rsamtools")
+
+# install seqLogo
+BiocManager::install("seqLogo")
+
+# install seqLogo
+BiocManager::install("seqbias")
+
+# install seqLogo
+install.packages("ggplot")
